0f5656581e88f6ef3a6f855c4fc4159527373342
Dylan Moonfire <dylan.moonfire@ryan.com>
2015-08-14 16:12:20 -0500

    More work, more examples.

--- spfbo.js
+++ spfbo.js
@@ -1,3 +1,89 @@
+function appendKeyValue(label, value, parent) {
+	if (value)
+	{
+		$("<div class='row'><div class='col-md-2 metadata-key'>" + label + ":</div><div class='col-md-10 metadata-value'>" + value + "</div></div>")
+			.appendTo(parent);
+	}
+}
+
+function appendKeyUrls(label, urls, parent) {
+	// If we don't have urls, then skip it.
+	if (!urls)
+	{
+		return;
+	}
+	
+	// Create the elements we'll append to.
+	var row = $("<div class='row'></div>");
+	var key = $("<div class='col-md-2 metadata-key'>" + label + "</div>");
+	var values = $("<div class='col-md-10 metadata-value'></div>");
+	
+	row.append(key);
+	row.append(values);
+	parent.append(row);
+
+	// Go through each URL and add the entry.
+	for (url of urls)
+	{
+		// Figure out the actual url.
+		var u = url.url;
+		var t = u;
+		
+		if (url.title)
+		{
+			t = url.title;
+		}
+		
+		switch (url.type)
+		{
+			case "twitter":
+				u = "https://twitter.com/" + url.id;
+				t = "Twitter";
+				break;
+				
+			case "amazon":
+				var a = "<a href='https://www.amazon.com/dp/" + url.asin + "'>Amazon US</a>";
+				a += " <a href='https://www.amazon.co.uk/dp/" + url.asin + "'>UK</a>";
+				a += "<br/>"
+				values.append(a);
+				continue;
+				
+			case "smashwords":
+				u = "https://www.smashwords.com/books/view/" + url.id;
+				t = "Smashwords";
+				break;
+				
+			case "goodreads":
+				u = "https://www.goodreads.com/book/show/" + url.id;
+				t = "Goodreads";
+				break;
+			
+			case "facebook":
+				u = "https://facebook.com/" + url.id;
+				t = "Facebook";
+				break;
+			
+			case "google+":
+				u = "https://plus.google.com/b/" + url.id + "/";
+				t = "Google+";
+				break;
+			
+			case "ello":
+				u = "https://ello.co/" + url.id;
+				t = "Ello";
+				break;
+			
+			case "reddit":
+				u = "https://www.reddit.com/u/" + url.id;
+				t = "Reddit";
+				break;
+		}
+		
+		// Add in the entry.
+		values.append("<a href='" + u + "'>" + t + "</a><br/>");
+	}
+}
+
 function onData(textData) {
 	// Parse the results as JSON.
 	var data = JSON.parse(textData);
@@ -17,8 +103,8 @@ function onData(textData) {
 			var nationality_slug = "nationality-" + book.nationality.toLowerCase().replace(/\s+/g, "-").replace("’", "");;
 			var group_slug = "group-" + reviewer.group.toLowerCase().replace(/\s+/g, "-").replace(/['’]/, "");;
 			
-			if (status_slug === "status-passed") { status_slug += " status-passed-hide"; }
-			if (gender_slug === "gender-unknown") { status_slug += " gender-unknown-hide"; }
+			//if (status_slug === "status-passed") { status_slug += " status-passed-hide"; }
+			//if (gender_slug === "gender-unknown") { status_slug += " gender-unknown-hide"; }
 			
 			// Figure out the panel based on the status.
 			var panelType = "panel-warning";
@@ -48,7 +134,7 @@ function onData(textData) {
 				+ "</div>");
 			var panelBodyContainer = $(
 				"<div id='" + slug
-				+ "' class='panel-collapse collapse in' aria-labelledby='"
+				+ "' class='panel-collapse collapse out' aria-labelledby='"
 				+ slug + "-title'></div>");
 			var panelBody = $("<div class='panel-body'></div></div>");
 				
@@ -76,8 +162,42 @@ function onData(textData) {
 			{
 				$(book.summary).appendTo(summary);
 			}
-			
 
+			// Add in the metadata
+			appendKeyValue("Words", book.words, summary);
+			appendKeyValue("POV", book.pov, summary);
+			appendKeyValue("Tense", book.tense, summary);
+			
+			if (book.urls)
+			{
+				appendKeyUrls("Reviews", book.urls.reviews, summary);
+				appendKeyUrls("Links", book.urls.book, summary);
+			}
+			
+			// Add in the author information.
+			summaryRow = $("<div class='row'></div>");
+			cover = $("<div class='col-md-2'></div>");
+			summary = $("<div class='col-md-10'></div>");
+			
+			panelBody.append(summaryRow);
+			summaryRow.append(cover);
+			summaryRow.append(summary);
+			
+			if (book.urls && book.urls.avatar)
+			{
+				$("<img class='img-responsive' src='" + book.urls.avatar + "' alt='" + book.author + " picture'/>")
+					.appendTo(cover);
+			}
+			
+			if (book.about)
+			{
+				$(book.about).appendTo(summary);
+			}
+			
+			if (book.urls)
+			{
+				appendKeyUrls("Links", book.urls.author, summary);
+			}
 			/*
 <div class="panel panel-default">
     <div class="panel-heading" role="tab" id="headingOne">
